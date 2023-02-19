b6c7e047d119bd04b0ad05c267cccf667093bde6
Boriss Varlamovs <bvarlamo@c2s12d63.42wolfsburg.de>
2023-02-19 14:17:13 +0100

    almost finished

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -15,4 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 require_once ABSPATH . 'wp-settings.php';
 EOF
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 fi
+/usr/sbin/php-fpm8 -F
